ae2b64e3eb2b4c2414939511ce3e7d214615e8e3
RIN <ericalexli@gmail.com>
2022-03-03 07:32:19 -0500

    Update main.py

--- N-body Diagram Simulation/main.py
+++ N-body Diagram Simulation/main.py
@@ -1,9 +1,11 @@
 import random, pygame as pg
 
-G = 1
+G = 6.05
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 BODIES = []
+ZOOM = 1
+TIME = 0
 
 class mass():
       global G, BODIES
@@ -15,8 +17,9 @@ class mass():
             self.dx = dx
             self.dy = dy
             self.trail = []
+            for i in range(255):
+                  self.trail.append( (self.x, self.y) )
             self.stable=stable
-            self.deactivated = False
             if radius != None:
                   self.r = radius
 
@@ -42,17 +45,16 @@ class mass():
             return(((self.x-other.x)**2+(self.y-other.y)**2)**0.5)
 
       def step(self):
-            if self.deactivated:
-                  return
             dx = self.get_acceleration()[0]
             dy = self.get_acceleration()[1]
             
             self.dx += dx
             self.dy += dy
 
-            self.trail.append((self.x,self.y))
-            if len(self.trail) > 300:
-                  del self.trail[0]
+            if TIME%15 == 0:
+                  self.trail.append((self.x,self.y))
+                  if len(self.trail) > 1000:
+                        del self.trail[0]
 
       def check_collisions(self):
             for b in range(len(BODIES)):
@@ -72,13 +74,14 @@ class mass():
                         return True
             return False
 
-def main():
-      global BLACK, WHITE, BODIES
-
-      N = 10 # number of bodies
+def init_bodies(N=10,clear=True,multi=1):
+      global BODIES
+      if clear:
+            BODIES.clear()
+      #BODIES = [mass(10000,(0,0),0,0)]
       for i in range(N-1):
             rand = random.randrange(40,100)
-            temp = mass(50,(random.randrange(300,700),random.randrange(300,700)),random.random()-0.5,random.random()-0.5)
+            temp = mass(50,(random.randrange(-500,500),random.randrange(-500,500)),multi*(random.random()-0.5),multi*(random.random()-0.5))
             BODIES.append(temp)
       # balance
       bdx = 0
@@ -90,9 +93,23 @@ def main():
             bdy -= b.dy
             massX+=b.x
             massY+=b.y
-      print(bdx,bdy)
-      BODIES.append(mass(50,(500*N-massX,500*N-massY),bdx,bdy))
-      #BODIES = [mass(500,(500,500),0,-.2),mass(100,10,(700,500),0,1)
+      BODIES.append(mass(50,(-1*massX,-1*massY),bdx,bdy))
+      BODIES = [mass(121,(-970,243),0.46,0.37),mass(121,(970,-243),0.46,0.37),]
+      bdx = 0
+      bdy = 0
+      massX=0
+      massY=0
+      for b in BODIES:
+            bdx -= b.dx
+            bdy -= b.dy
+            massX+=b.x
+            massY+=b.y
+      BODIES.append(mass(121,(-1*massX,-1*massY),bdx,bdy))
+
+def main():
+      global BLACK, WHITE, BODIES, ZOOM, TIME
+
+      init_bodies(10)
 
       pg.init()
       
@@ -104,28 +121,33 @@ def main():
 
       # show count
       font = pg.font.Font('freesansbold.ttf', 32)
-      text = font.render(str(len(BODIES)), True, WHITE, BLACK)
+      text = font.render(str(len(BODIES))+"  "+str(round(100*ZOOM,2))+"%", True, WHITE, BLACK)
       textRect = text.get_rect()
-      textRect.center = (50,50)
+      textRect.center = (100,50)
       
       while True:
-            clock.tick(60)
+            ZOOM = max(ZOOM,0.001)
+            ZOOM = min(ZOOM,4)
+            clock.tick(600)
+            TIME+=1
             screen.fill(BLACK)
-            text = font.render(str(len(BODIES)), True, WHITE, BLACK)
+            text = font.render(str(len(BODIES))+"  "+str(round(100*ZOOM,2))+"%", True, WHITE, BLACK)
             screen.blit(text, textRect)
             massx = 0
             massy = 0
             total = 0
+
             for b in BODIES:
                   b.step()
-                  pg.draw.circle(screen,WHITE,(int(b.x),int(b.y)),int(b.r),int(b.r))
-                  for t in b.trail:
-                        pg.draw.circle(screen,(0,100,255),(int(t[0]),int(t[1])),1,1)
+                  pg.draw.circle(screen,WHITE,(int(b.x*ZOOM)+500,int(b.y*ZOOM)+500),int(ZOOM*b.r),int(ZOOM*b.r))
+                  for t in range(len(b.trail)):
+                        pg.draw.circle(screen,(min(t,255),min(t,255),min(t,255)),(int(b.trail[t][0]*ZOOM)+500,int(b.trail[t][1]*ZOOM)+500),1,1)
                   
                   # center of mass
                   total+=b.m
                   massx+=b.x*b.m
                   massy+=b.y*b.m
+
             while b.check_collisions():
                   pass
             for b in range(len(BODIES)-1,-1,-1):
@@ -134,11 +156,19 @@ def main():
                   if not b.stable:
                         b.x += b.dx
                         b.y += b.dy
-            pg.draw.circle(screen,(255,100,100),(int(massx/total),int(massy/total)),2,2)
+
+            pg.draw.circle(screen,(255,100,100),(int(ZOOM*massx/total)+500,int(ZOOM*massy/total)+500),4,2)
             #pg.draw.circle(screen,(255,50,50),(500,500),3,3)
 
             
+            keys = pg.key.get_pressed()
+            ZOOM += (keys[pg.K_UP]-keys[pg.K_DOWN]) * ZOOM/100
             for event in pg.event.get():
+                  if event.type == pg.KEYDOWN:
+                        if event.key == pg.K_r:
+                              init_bodies()
+                        if event.key == pg.K_a:
+                              init_bodies(4,False)
                   if event.type == pg.QUIT:
                         pg.quit()
                         quit()
